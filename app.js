32d51cd370257112b0bf723485495426e7e63160
nijhum0891@gmail.com <nijhum0891@gmail.com>
2022-10-01 22:03:01 +0600

    b

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function makeArry(){
 }
 function CreateSquare(){
     return (
-        <div class='w-[100vw] p-6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6'>
+        <div class='w-[100vw] p-5 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5'>
             {makeArry()}
         </div>
     )
