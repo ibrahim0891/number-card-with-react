5beaf9dc13b090afa2498aeb6be5994c8762d599
nijhum0891@gmail.com <nijhum0891@gmail.com>
2022-10-01 22:37:47 +0600

    for i in ranger(1,10){print('helllo world')}

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 function MakeSquare(props) {
-    return <div class='scale flex-auto flex items-center justify-center w-full aspect-[1/1] border boder'> {props.number}</div>
+    return <div class='scale  flex-auto flex items-center justify-center w-full aspect-[1/1] border boder'> {props.number}</div>
 }
 var i = 1
 function makeArry(){
     var square = []
-    for (i; i <= 30; i++) {
+    for (i; i <= 40; i++) {
         square.push(<MakeSquare number={i}/>)
-    } 
+    }
+    console.log(square)
     return square
 }
 function CreateSquare(){
     return (
         <div class='w-[100vw] p-7 grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-4 gap-7'>
-            {makeArry()}
+            {makeArry() }
         </div>
     )
 }
