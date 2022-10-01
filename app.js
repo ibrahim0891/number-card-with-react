5794b0925d7457a0fadf65f5a080f652467203c9
nijhum0891@gmail.com <nijhum0891@gmail.com>
2022-10-01 21:59:00 +0600

    responsive data update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function makeArry(){
 }
 function CreateSquare(){
     return (
-        <div class='w-[100vw] p-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6'>
+        <div class='w-[100vw] p-6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6'>
             {makeArry()}
         </div>
     )
